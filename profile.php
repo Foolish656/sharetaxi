8ed11569d2354031f50e52964fbce0b7531f3497
Foolish656 <leofrost343@gmail.com>
2018-02-21 09:10:09 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,11 +1,11 @@
 <?php
    include('session.php');
 
-    if(!isset($user_id)){
+    if(!isset($userId)){
         header("location: login.php");
     }
 	
-	$sql = "SELECT * FROM pool WHERE user_id = '$user_id'";
+	$sql = "SELECT * FROM pool WHERE user_id = '$userId'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 	$count = mysqli_num_rows($result);
@@ -18,7 +18,7 @@
    </head>
    
    <body>
-      <h1>Welcome <?php echo $login_session; ?></h1>
+      <h1>Welcome <?php echo $loginSession; ?></h1>
 		<h2><a href = "welcome.php">Homepage</a></h2>
         <h2><a href = "logout.php">Sign Out</a></h2>
         <h2><a href = "settings.php">Settings</a></h2>
