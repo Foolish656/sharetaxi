d58e09ae220f76978a9b126551b85d1d79f97c3e
Foolish656 <leofrost343@gmail.com>
2018-02-21 09:46:28 +0800

    Update session.php

--- session.php
+++ session.php
@@ -6,13 +6,13 @@
 
    $id = $_SESSION['id'];
    
-   $ses_sql = mysqli_query($db,"select name, user_id from users where user_id = {$id}");
+   $sesSql = mysqli_query($db,"select name, user_id from users where user_id = {$id}");
    
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   $row = mysqli_fetch_array($sesSql,MYSQLI_ASSOC);
    
    $loginSession = $row['name'];
 
-   $userId = $row['userId'];
+   $userId = $row['user_id'];
    
    if(!isset($_SESSION['loginUser'])){
       header("location:login.php");
